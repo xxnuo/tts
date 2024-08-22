db8c886cb9e1bff92a52a571551897feb3fdbe17
Kerry <54252779+xxnuo@users.noreply.github.com>
2024-08-22 10:04:36 +0800

    添加 cors 支持

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 	"tts/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 
 	// 加载模板文件
 	router.LoadHTMLGlob("templates/*")
@@ -17,4 +21,20 @@ func SetupRouter() *gin.Engine {
 	router.GET("/", handlers.Index)
 
 	return router
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // 允许所有域名请求
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
 }
